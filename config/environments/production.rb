c37846953f5812b296964867a12aa4a3ac081b2f
Akbar Priyono Haryadi <akbarharyadi@outlook.com>
2025-10-23 13:05:21 +0700

    set up production setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,13 +36,17 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [
+    "https://api-chat.akbarharyadi.com",
+    "https://chat.akbarharyadi.com"
+  ]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
